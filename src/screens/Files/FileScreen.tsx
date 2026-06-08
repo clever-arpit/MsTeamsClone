b3ad71a916fac3ff4193ed2dad2f30ecb7bce32f
Arpit Dhiman <105314152+clever-arpit@users.noreply.github.com>
2026-06-08 23:31:34 +0530

    new changes

---
+++ src/screens/Files/FileScreen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const FileScreen = () => {
+  return (
+    <View>
+      <Text>FileScreen</Text>
+    </View>
+  )
+}
+
+export default FileScreen
+
+const styles = StyleSheet.create({})
